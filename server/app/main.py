e3fd0af9b0852f499b56bec40eb4f0302009d51b
LukaVucinic <159730295+LukaVucinic@users.noreply.github.com>
2025-10-03 03:10:51 +0200

    Update main.py

--- server/app/main.py
+++ server/app/main.py
@@ -7,7 +7,11 @@ app = FastAPI(title="DevMatch API", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",     
+        "http://127.0.0.1:5173",     
+        "https://devmatch-phi.vercel.app"  
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
